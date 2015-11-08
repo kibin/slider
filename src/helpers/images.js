f5da8c02e98a5f8f6902cb3f5039cb0b48964943
kibin ᕕ( ᐛ )ᕗ <mr.kibin@gmail.com>
2015-11-08 23:33:28 +0300

    canonical images

--- src/helpers/images.js
+++ src/helpers/images.js
@@ -1,6 +1,13 @@
+// export const images = [
+//   `//40.media.tumblr.com/fec4ee9dadb1f55763813c9eb0061159/tumblr_nwnhovdks21tfs2oyo1_1280.jpg`,
+//   `//40.media.tumblr.com/da84456cdd21318a3d8b1744ed92e1fc/tumblr_nx6unxzF4O1tfyylco1_1280.jpg`,
+//   `//40.media.tumblr.com/c6602301265a9091a5db2043c434edd1/tumblr_nx6mbzgRuk1tfyylco1_1280.jpg`,
+//   `//36.media.tumblr.com/dfdb16f19123a21161a254203d70b577/tumblr_nxaanpCeEO1ud3wmzo1_1280.jpg`,
+// ];
+
 export const images = [
-  `//40.media.tumblr.com/fec4ee9dadb1f55763813c9eb0061159/tumblr_nwnhovdks21tfs2oyo1_1280.jpg`,
-  `//40.media.tumblr.com/da84456cdd21318a3d8b1744ed92e1fc/tumblr_nx6unxzF4O1tfyylco1_1280.jpg`,
-  `//40.media.tumblr.com/c6602301265a9091a5db2043c434edd1/tumblr_nx6mbzgRuk1tfyylco1_1280.jpg`,
-  `//36.media.tumblr.com/dfdb16f19123a21161a254203d70b577/tumblr_nxaanpCeEO1ud3wmzo1_1280.jpg`,
-];
+  `http://challenge.publitas.com/images/0.jpg`,
+  `http://challenge.publitas.com/images/1.jpg`,
+  `http://challenge.publitas.com/images/2.jpg`,
+  `http://challenge.publitas.com/images/3.jpg`,
+]
